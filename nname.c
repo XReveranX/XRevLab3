d82b3ec4a0333269a1107b6980549b4aaa7824f4
user <user@ws1013402145.pm>
2024-11-28 12:44:01 +0500

    result1

--- nname.c
+++ nname.c
@@ -4,25 +4,38 @@
 
 int m1=0;
 int m2=0;
+int sum=0;
+int mi=0;
+int mii=0;
+int miii = 0;
+int n=0;
 
-void fill(int n, int a[])
+void output(int a[])
+{
+    int i;
+    for (i=0;i<n;i++)
+        printf("%i: %i \n", i, a[i]);
+    printf("\nдва максимальных числа под индексами %i %i: %i %i \nсумма = %i", mi,(mi+1), a[mi], a[(mi+1)], sum);
+    printf("\nдва минимальных числа под индексами %i %i: %i %i", miii, mii, m1, m2);
+    printf("\nрезультат работы программы: %i %i \n", (sum+m1),(sum+m2));
+}
+
+void fill(int a[])
 {
     int i;
     for (i=0;i<n;i++)
         a[i] = rand() % 101 - 50;
 }
 
-int findmaxsum(int n, int a[])
+int findmaxsum(int a[])
 {
-int m=0;
 for(int i=0;i<(n-1);i++)
 {
-    if((a[i]+a[i+1])>m){m=(a[i]+a[i+1]);}
+    if((a[i]+a[i+1])>sum){sum=(a[i]+a[i+1]); mi=i;}
 }
-return(m);
 }
 
-void findmintwo(int n, int a[])
+void findmintwo(int a[])
 {
 m1=a[0];
 m2=a[1];
@@ -32,6 +45,8 @@ for(int i=0;i<n;i++)
     {
         m2=m1;
         m1=a[i];
+        mii=miii;
+        miii=i;
     }
 }
 }
@@ -39,12 +54,11 @@ for(int i=0;i<n;i++)
 int main()
 {
     srand(time(NULL));
-int n;
 printf("Введите число элементов массива \n");
 scanf("%i", &n);
 int A[n];
-fill(n, A);
-int sum=findmaxsum(n, A);
-findmintwo(n, A);
-printf("%i %i \n",(m1+sum), (m2+sum));
+fill(A);
+findmaxsum(A);
+findmintwo(A);
+output(A);
 }
